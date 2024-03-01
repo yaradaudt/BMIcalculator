43c7c49549eba161ad7bcae94615d7fd0cc6aae1
yaradaudt <157490089+yaradaudt@users.noreply.github.com>
2024-03-01 19:35:59 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+import { Modal } from './modal.js'
+import { alertError } from './alert-error.js'
+import { calculateIMC, notANumber } from './utils.js'
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = event => {
+    event.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+
+    if (weightOrHeightIsNotANumber) {
+        alertError.open()
+        return;
+    }
+
+    alertError.close()
+
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
+    
+}
